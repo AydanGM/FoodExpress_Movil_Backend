7d2799a5d8b0e15fdcf0d6c63d3d539f7ac1a6e5
AydanGM <ayd.gonzalez@duocuc.cl>
2025-11-21 20:11:12 -0300

    Commit Inicial

---
+++ src/main/kotlin/com/example/foodexpress_backend/FoodExpressBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.foodexpress_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodExpressBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodExpressBackendApplication>(*args)
+}
